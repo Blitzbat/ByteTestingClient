0490d291ab45f744a34a128d9a60a89bc4001596
Sebastian Parwolowski <sebastian@bitwantsbyte.com>
2016-02-18 23:38:19 +0100

    Initial commit

---
+++ typings/browser.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="browser/ambient/Q/Q.d.ts" />
+/// <reference path="browser/ambient/commander/commander.d.ts" />
+/// <reference path="browser/ambient/glob/glob.d.ts" />
+/// <reference path="browser/ambient/node/node.d.ts" />
+/// <reference path="browser/ambient/socket.io-client/socket.io-client.d.ts" />
+/// <reference path="browser/ambient/socket.io/socket.io.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="main/ambient/Q/Q.d.ts" />
+/// <reference path="main/ambient/commander/commander.d.ts" />
+/// <reference path="main/ambient/glob/glob.d.ts" />
+/// <reference path="main/ambient/node/node.d.ts" />
+/// <reference path="main/ambient/socket.io-client/socket.io-client.d.ts" />
+/// <reference path="main/ambient/socket.io/socket.io.d.ts" />
